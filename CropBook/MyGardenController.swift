2eacc1fe525986b339afe61f5f4a6139cc4d19e4
jon <jon@Julianas-MacBook-Pro.local>
2018-06-28 19:26:11 -0700

    Empty Tab Interface

---
+++ CropBook/MyGardenController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  CropBook
+//
+//  Created by jon on 2018-06-28.
+//  Copyright © 2018 CMPT276-Group15. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Page 1 Loaded")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
