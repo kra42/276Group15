83187b8712470746839fdaba405857cf26eede43
jon <jon@Julianas-MacBook-Pro.local>
2018-07-02 00:34:28 -0700

    Improved the Weather object

---
+++ CropBook/CropProfile.swift
@@ -0,0 +1,58 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+class CropProfile
+{
+//varaibles for crop attributes
+    var cropid: Int
+    var cropName: String
+    var cropType: String
+    var wateringVariable: Float
+    
+    
+//object seperation step
+
+    init (cropid: Int,cropName: String, cropType: String, wateringVariable: Float)
+    {
+        self.cropid = cropid
+        self.cropName = cropName
+        self.cropType = cropType
+        self.wateringVariable = wateringVariable
+        
+    }
+    
+
+    //funtion to get attributes of different objects
+    func GetCropid()-> Int
+    {
+        return self.cropid
+    }
+    
+    func GetCropName()-> String
+    {
+        return self.cropName
+    }
+    
+    func GetCropType()-> String
+    {
+        return self.cropType
+    }
+    
+    func GetWateringVariable()-> Float
+    {
+        return self.wateringVariable
+    }
+    
+}
+
+
+var MyCrop = CropProfile(cropid: 12, cropName: "Carrot", cropType: "root", wateringVariable: 0.78)
+print(MyCrop.GetCropid())
+print(MyCrop.GetCropName())
+print(MyCrop.GetCropType())
+print(MyCrop.GetWateringVariable())
+
+
