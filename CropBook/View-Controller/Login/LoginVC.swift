890ee188cd5e43c93d6d3791cb9363f81b1292e3
jon <jon@d207-023-194-083.wireless.sfu.ca>
2018-07-18 14:36:30 -0700

    Removed secret button, Added login warning

--- CropBook/View-Controller/Login/LoginVC.swift
+++ CropBook/View-Controller/Login/LoginVC.swift
@@ -39,20 +39,17 @@ class LoginVC: UIViewController {
                 
             }else{
                 print("Error : \(error!.localizedDescription)")
+                let alert = UIAlertController(title: "Inavlid Username/Password", message: "", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (action) in
+                    alert.dismiss(animated:true, completion:nil)
+                }))
+                
+                self.present(alert, animated:true, completion:nil)
             }
         }
     }
     
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
